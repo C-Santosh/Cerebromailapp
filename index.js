82e4c9df58f84b8a10452be385371d7c9c63d0a8
C-Santosh <107548634+C-Santosh@users.noreply.github.com>
2025-06-18 19:56:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/send-email', async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`📡 Server running on http://localhost:${PORT}`);
 });
